0377a68fcc9586e3957fb3778e3eae1f6d220ed4
Suit9uy <129301387+Suit9uy@users.noreply.github.com>
2025-04-23 16:51:38 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, rust!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4); // ✅ 基本測試，保證至少有一個 test 可跑
+    }
 }
